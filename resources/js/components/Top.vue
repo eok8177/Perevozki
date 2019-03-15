9230af2e720da9da6e6ed5efd7295dbdc3c45c99
Evgen <eok8177@gmail.com>
2019-03-15 17:23:18 +0200

    cutups with routes

---
+++ resources/js/components/Top.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="header main-header page-header">
+    <div class="wrapper ">
+      <div class="header__wrapper">
+        <router-link to="/" class="header__logo">
+          <img src="img/logo-header.svg" alt="Logo">
+        </router-link>
+        <button class="open">Open</button>
+        <div class="header__block">
+          <ul class="header__tels">
+            <li class="header__tels-item"><a href="#" class="header__tels-link">(044) 362 78 38</a></li>
+            <li class="header__tels-item"><a href="#" class="header__tels-link">(067) 558 88 38</a></li>
+            <li class="header__tels-item"><a href="#" class="header__tels-link">(093) 518 88 38</a></li>
+            <li class="header__tels-item"><a href="#" class="header__tels-link">(050) 458 88 38</a></li>
+            <a href="#" class="navigation__hidden button">Заказать звонок</a>
+          </ul>
+          <nav class="header__navigation">
+            <router-link to="/" exact class="navigation__item" active-class="navigation__item--active">Главная</router-link>
+            <router-link to="/avtopark" class="navigation__item" active-class="navigation__item--active">Автопарк</router-link>
+            <router-link to="/about-us" class="navigation__item" active-class="navigation__item--active">О нас</router-link>
+            <router-link to="/reviews" class="navigation__item" active-class="navigation__item--active">Отзывы</router-link>
+            <router-link to="/tips" class="navigation__item" active-class="navigation__item--active">Советы</router-link>
+            <a href="#" class="navigation__call button button-white">Заказать звонок</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+
+  export default {
+    name: 'Top'
+  }
+</script>
---
+++ resources/views/spa.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/plugins/slick/slick.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Aleo" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="app"></div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="/plugins/jquery-1.8.1.js"></script>
+    <script src="/plugins/slick/slick.min.js"></script>
+    <script src="/js/common.js"></script>
+
+</body>
+</html>
