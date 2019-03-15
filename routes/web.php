44d851a9bd4da9ba1b3890cb7dc8eac39e41ed36
Evgen <eok8177@gmail.com>
2019-03-15 17:22:48 +0200

    route to SPA

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/{any}', function () {
+    return view('spa');
+})->where('any', '.*');
