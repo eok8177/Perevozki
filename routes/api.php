d38e5e827c041b74df976b739e1d10adea72c4c6
Evgen <eok8177@gmail.com>
2019-03-22 18:59:03 +0200

    test API

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function() {
+
+    Route::get('/tips',  ['uses' => 'TestController@tips']);
+    Route::get('/tip/{id}',  ['uses' => 'TestController@tip']);
+
+
+});
