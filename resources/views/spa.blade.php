da753ec96ce2b11b94104cc30de33031f9d104fd
Evgen <eok8177@gmail.com>
2019-03-22 20:47:35 +0200

    optimize for google speed

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/plugins/slick/slick.css"/>
-    <link href="https://fonts.googleapis.com/css?family=Aleo" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.min.css">
     <title>Document</title>
 </head>
 
@@ -18,6 +16,20 @@
     <script src="/plugins/jquery-1.8.1.js"></script>
     <script src="/plugins/slick/slick.min.js"></script>
     <script src="/js/common.js"></script>
+
+    <script>
+        WebFontConfig = {
+          google: { families: [ 'Aleo:400' ] }
+        };
+        (function() {
+          var wf = document.createElement('script');
+          wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+          wf.type = 'text/javascript';
+          wf.async = 'true';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(wf, s);
+        })(); 
+    </script>
 
 </body>
 </html>
