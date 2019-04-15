4beb4d02f7ba696cb58781563df41aff381e4e85
Evgen <eok8177@gmail.com>
2019-04-15 18:30:28 +0300

    sidebar; css changes

--- resources/views/backend/layouts/admin.blade.php
+++ resources/views/backend/layouts/admin.blade.php
@@ -85,7 +85,7 @@
 {{-- <script src="{{ asset('vendors/jvectormap/jquery-jvectormap-us-aea-en.js') }}" type="text/javascript"></script> --}}
 <script src="{{ asset('vendors/summernote/dist/summernote.min.js') }}" type="text/javascript"></script>
 <!-- CORE SCRIPTS-->
-<script src="{{ asset('backend/js/app.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('backend/js/app.js') }}" type="text/javascript"></script>
 <!-- PAGE LEVEL SCRIPTS-->
 <script src="/vendor/laravel-filemanager/js/lfm.js"></script>
 {{-- <script src="{{ asset('backend/js/scripts/dashboard_1_demo.js') }}" type="text/javascript"></script> --}}
--- resources/views/backend/layouts/includes/sidebar.blade.php
+++ resources/views/backend/layouts/includes/sidebar.blade.php
@@ -7,49 +7,52 @@
             <div class="font-strong">James Brown</div><small>Administrator</small></div>
     </div>
     <ul class="side-menu metismenu">
-        <li>
+        {{-- <li>
             <a class="active" href="index.html"><i class="sidebar-item-icon fa fa-th-large"></i>
                 <span class="nav-label">Dashboard</span>
             </a>
-        </li>
+        </li> --}}
         <li class="heading">Menu</li>
         <li>
             <a href="/admin/pages/statics">
-                <i class="sidebar-item-icon fa fa-smile-o"></i>
+                <i class="sidebar-item-icon fa fa-newspaper-o"></i>
                 <span class="nav-label">Страницы</span>
             </a>
         </li>
         <li>
             <a href="/admin/pages/services">
-                <i class="sidebar-item-icon fa fa-smile-o"></i>
+                <i class="sidebar-item-icon fa fa-truck"></i>
                 <span class="nav-label">Услуги</span>
             </a>
         </li>
         <li>
             <a href="/admin/pages/cars">
-                <i class="sidebar-item-icon fa fa-smile-o"></i>
+                <i class="sidebar-item-icon fa fa-car"></i>
                 <span class="nav-label">Машины</span>
             </a>
         </li>
         <li>
             <a href="/admin/reviews">
-                <i class="sidebar-item-icon fa fa-smile-o"></i>
-                <span class="nav-label">Reviews</span>
+                <i class="sidebar-item-icon fa fa-comments-o"></i>
+                <span class="nav-label">Отзывы</span>
             </a>
         </li>
         <li>
-            <a href="javascript:;">
-                <i class="sidebar-item-icon fa fa-archive"></i>
-                <span class="nav-label">Catalog</span><i class="fa fa-angle-left arrow"></i>
-            </a>
-            <ul class="nav-2-level collapse">
-                <li>
+            {{-- <a href="javascript:;"> --}}
+                {{-- <i class="sidebar-item-icon fa fa-archive"></i> --}}
+                {{-- <span class="nav-label">Catalog</span><i class="fa fa-angle-left arrow"></i> --}}
+            {{-- </a> --}}
+            {{-- <ul class="nav-2-level collapse"> --}}
+{{--                 <li>
                     <a href="/admin/categories">Categories</a>
-                </li>
-                <li>
-                    <a href="/admin/posts">Posts</a>
-                </li>
-            </ul>
+                </li> --}}
+                {{-- <li> --}}
+                    <a href="/admin/posts">
+                        <i class="sidebar-item-icon fa fa-file-text-o"></i>
+                        <span class="nav-label">Советы</span>
+                    </a>
+                {{-- </li> --}}
+            {{-- </ul> --}}
         </li>
         <li>
             <a href="javascript:;">
